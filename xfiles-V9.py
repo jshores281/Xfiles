c9a64265a9703bb5cda6d5b22c9103b3251e33d7
jshores281 <52839097+jshores281@users.noreply.github.com>
2022-06-17 15:19:07 -0500

    Update xfiles-V9.py

--- xfiles-V9.py
+++ xfiles-V9.py
@@ -1,21 +1,3 @@
-###############################################################################
-#! Python 3.9.4/xfiles_mon.Python										
-# version 4																
-###############################################################################
-# monitors files and top level folders in a user specified directory 	
-# if any files or folders are added or deleted from the directory, 		
-# a email to sms alert system will trigger of what items in directory changed		
-###############################################################################
-# secured with tls
-###############################################################################
-###############################################################################
-# FEATURES TO ADD 		
-# ~ add functions as members to a single class?
-# ~ copy new and removed files and folders in srcfolder to dstfolder !!!!!!
-# ~ package into a GUI
-###############################################################################
-###############################################################################
-
 #!/bin/python3
 
 import os, sys, time, smtplib
